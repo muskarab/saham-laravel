31996ea8d702e00499091ba3b9488f04c3ddc715
muskarab <mustafa.kamal.mk858@gmail.com>
2021-12-23 14:25:00 +0700

    Update user controller can update value vektor s and v depends bobot user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bobot;
 use App\Models\Emiten;
 use App\Models\InstrumentSaham;
 use App\Models\User;
@@ -135,15 +136,171 @@ class UserController extends Controller
             }
         }
 
-            // $user = DB::table('users')->where('id', $user->id)->update(['name' => request('name'),
-            //     'name' => request('name'),
-            //     'email' => request('email'),
-            //     'address' => request('address'),
-            //     'date_of_birth' => request('date_of_birth'),
-            //     'role' => request('role'),
-            // ]);
-            // $user->update($request->all());
-            // dd($request->all());
+        $users = User::get();
+        $emiten_kons = Emiten::where('index_id', 1)->get();
+        $emiten_syars = Emiten::where('index_id', 2)->get();
+        $lastdata_emiten = Emiten::orderBy('id', 'DESC')->first();
+        $bobots = Bobot::where('instrument_saham_id','=', 1)->get();
+        foreach ($users as $user) {
+            if ($user->instrument_saham_id == 1) {
+                //update vektor s
+                foreach ($emiten_kons as $emiten_kon) {
+                    $w_user_total = $user->w_eps_kon + $user->w_roe_kon + $user->w_per_kon;
+                    $w_eps = pow($emiten_kon->prefereni_kriteria['eps_pk'], - ($user['w_eps_kon'] / $w_user_total));
+                    $w_roe = pow($emiten_kon->prefereni_kriteria['roe_pk'], ($user['w_roe_kon'] / $w_user_total));
+                    $w_per = pow($emiten_kon->prefereni_kriteria['per_pk'], ($user['w_per_kon'] / $w_user_total));
+                    $w_total = $w_eps * $w_roe * $w_per;
+                    DB::table('vektor_s')
+                    ->where('user_id', $user->id)
+                        ->where('emiten_id', $emiten_kon->id)
+                        ->update([
+                            'vektor_s' => $w_total,
+                            'updated_at' => now()
+                        ]);
+                }
+                //update vektor v
+                $sum_vektor_s_kon = DB::table('emitens')
+                    ->join('vektor_s', 'emitens.id', '=', 'vektor_s.emiten_id')
+                    ->where('index_id', 1)
+                    ->where('vektor_s.user_id', $user->id)
+                    ->select('vektor_s.vektor_s')
+                    ->sum('vektor_s.vektor_s');
+                $vektor_s_kons = DB::table('emitens')
+                ->join('vektor_s', 'emitens.id', '=', 'vektor_s.emiten_id')
+                ->where('index_id', 1)
+                    ->where('vektor_s.user_id', $user->id)
+                    ->select(
+                        'emitens.*',
+                        'vektor_s.user_id',
+                        'vektor_s.vektor_s'
+                    )
+                    ->get();
+                foreach ($vektor_s_kons as $vektor_s_kon) {
+                    DB::table('vektor_v_s')
+                        ->where('user_id', $user->id)
+                        ->where('emiten_id', $vektor_s_kon->id)
+                        ->update([
+                            'vektor_v' => $vektor_s_kon->vektor_s / $sum_vektor_s_kon,
+                            'updated_at' => now()
+                        ]);
+                }
+            }
+            if ($user->instrument_saham_id == 2) {
+                //update vektor s
+                foreach ($emiten_syars as $emiten_syar) {
+                    $w_user_total = $user->w_eps_syar + $user->w_roe_syar + $user->w_der_syar;
+                    $w_eps = pow($emiten_syar->prefereni_kriteria['eps_pk'], ($user['w_eps_syar'] / $w_user_total));
+                    $w_roe = pow($emiten_syar->prefereni_kriteria['roe_pk'], ($user['w_roe_syar'] / $w_user_total));
+                    $w_der = pow($emiten_syar->prefereni_kriteria['der_pk'], ($user['w_der_syar'] / $w_user_total));
+                    $w_total = $w_eps * $w_roe * $w_der;
+                    DB::table('vektor_s')->where('user_id', $user->id)->where('emiten_id', $emiten_syar->id)->update([
+                        'vektor_s' => $w_total,
+                        'updated_at' => now()
+                    ]);
+                }
+                //update vektor v
+                $sum_vektor_s_syar = DB::table('emitens')
+                ->join('vektor_s', 'emitens.id', '=', 'vektor_s.emiten_id')
+                    ->where('index_id', 2)
+                    ->where('vektor_s.user_id', $user->id)
+                    ->select('vektor_s.vektor_s')
+                    ->sum('vektor_s.vektor_s');
+                $vektor_s_syars = DB::table('emitens')
+                ->join('vektor_s', 'emitens.id', '=', 'vektor_s.emiten_id')
+                ->where('index_id',
+                    2
+                )
+                ->where('vektor_s.user_id', $user->id)
+                ->select('emitens.*', 'vektor_s.user_id', 'vektor_s.vektor_s')
+                ->get();
+                foreach ($vektor_s_syars as $vektor_s_syar) {
+                    DB::table('vektor_v_s')
+                    ->where('user_id', $user->id)
+                        ->where('emiten_id', $vektor_s_syar->id)
+                        ->update([
+                            'vektor_v' => $vektor_s_syar->vektor_s / $sum_vektor_s_syar,
+                            'updated_at' => now()
+                        ]);
+                }
+            }
+            if ($user->instrument_saham_id == 3) {
+                //update vektor s
+                foreach ($emiten_kons as $emiten_kon) {
+                    $w_user_total = $user->w_eps_kon + $user->w_roe_kon + $user->w_per_kon;
+                    $w_eps = pow($emiten_kon->prefereni_kriteria['eps_pk'], - ($user['w_eps_kon'] / $w_user_total));
+                    $w_roe = pow($emiten_kon->prefereni_kriteria['roe_pk'], ($user['w_roe_kon'] / $w_user_total));
+                    $w_per = pow($emiten_kon->prefereni_kriteria['per_pk'], ($user['w_per_kon'] / $w_user_total));
+                    $w_total = $w_eps * $w_roe * $w_per;
+                    DB::table('vektor_s')->where('user_id', $user->id)->where('emiten_id', $emiten_kon->id)->update([
+                        'vektor_s' => $w_total,
+                        'updated_at' => now()
+                    ]);
+                }
+                //update vektor v
+                $sum_vektor_s_kon = DB::table('emitens')
+                ->join('vektor_s', 'emitens.id', '=', 'vektor_s.emiten_id')
+                ->where('index_id', 1)
+                ->where('vektor_s.user_id', $user->id)
+                    ->select('vektor_s.vektor_s')
+                    ->sum('vektor_s.vektor_s');
+                $vektor_s_kons = DB::table('emitens')
+                ->join('vektor_s', 'emitens.id', '=', 'vektor_s.emiten_id')
+                ->where('index_id', 1)
+                    ->where('vektor_s.user_id', $user->id)
+                    ->select(
+                        'emitens.*',
+                        'vektor_s.user_id',
+                        'vektor_s.vektor_s'
+                    )
+                    ->get();
+                foreach ($vektor_s_kons as $vektor_s_kon) {
+                    DB::table('vektor_v_s')
+                        ->where('user_id', $user->id)
+                        ->where('emiten_id', $vektor_s_kon->id)
+                        ->update([
+                            'vektor_v' => $vektor_s_kon->vektor_s / $sum_vektor_s_kon,
+                            'updated_at' => now()
+                        ]);
+                }
+                //update vektor s
+                foreach ($emiten_syars as $emiten_syar) {
+                    $w_user_total = $user->w_eps_syar + $user->w_roe_syar + $user->w_der_syar;
+                    $w_eps = pow($emiten_syar->prefereni_kriteria['eps_pk'], ($user['w_eps_syar'] / $w_user_total));
+                    $w_roe = pow($emiten_syar->prefereni_kriteria['roe_pk'], ($user['w_roe_syar'] / $w_user_total));
+                    $w_der = pow($emiten_syar->prefereni_kriteria['der_pk'], ($user['w_der_syar'] / $w_user_total));
+                    $w_total = $w_eps * $w_roe * $w_der;
+                    DB::table('vektor_s')->where('user_id', $user->id)->where('emiten_id', $emiten_syar->id)->update([
+                        'vektor_s' => $w_total,
+                        'updated_at' => now()
+                    ]);
+                }
+                //update vektor v
+                $sum_vektor_s_syar = DB::table('emitens')
+                ->join('vektor_s', 'emitens.id', '=', 'vektor_s.emiten_id')
+                    ->where('index_id', 2)
+                    ->where('vektor_s.user_id', $user->id)
+                    ->select('vektor_s.vektor_s')
+                    ->sum('vektor_s.vektor_s');
+                $vektor_s_syars = DB::table('emitens')
+                ->join('vektor_s', 'emitens.id', '=', 'vektor_s.emiten_id')
+                ->where('index_id',
+                    2
+                )
+                ->where('vektor_s.user_id', $user->id)
+                ->select('emitens.*', 'vektor_s.user_id', 'vektor_s.vektor_s')
+                ->get();
+                foreach ($vektor_s_syars as $vektor_s_syar) {
+                    DB::table('vektor_v_s')
+                    ->where('user_id', $user->id)
+                        ->where('emiten_id', $vektor_s_syar->id)
+                        ->update([
+                            'vektor_v' => $vektor_s_syar->vektor_s / $sum_vektor_s_syar,
+                            'updated_at' => now()
+                        ]);
+                }
+            }
+        }
+
         return redirect()->route('user.index')->with('success', 'User updated success');
     }
 
--- resources/views/index_saham/index.blade.php
+++ resources/views/index_saham/index.blade.php
@@ -28,6 +28,7 @@
                     <thead class="thead-light">
                         <tr>
                             <th scope="col">No</th>
+                            <th scope="col">Tahun</th>
                             <th scope="col">Name</th>
                             <th scope="col">Instrument Saham</th>
                             <th scope="col"></th>
@@ -37,75 +38,9 @@
                         @forelse ($indexs as $index)
                             <tr>
                                 <td scope="row">{{ ++$i }}</td>
+                                <td>{{ $index->tahun }}</td>
                                 <td>{{ $index->name }}</td>
                                 <td>{{ $index->instrument_saham->name }}</td>
-                                <td class="text-right">
-                                    <div class="dropdown">
-                                        <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i class="fas fa-ellipsis-v text-blue"></i>
-                                        </a>
-                                        <form action="" method="POST">
-                                            <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                                                @csrf
-                                                @method('DELETE')
-                                                <a class="dropdown-item" href="">Update</a>
-                                                <button type="submit" class="dropdown-item" onclick="return confirm('Are you sure to delete this data?')">Delete</button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </td>
-                            </tr>
-                        @empty
-                            
-                        @endforelse
-                    </tbody>
-                </table>
-                </div>
-    
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <div class="card shadow">
-                <div class="card-header border-0">
-                    @if ($message = Session::get('success'))
-                        <div class="alert alert-success" role="alert">
-                            <strong>Success!</strong> {{ $message }}
-                        </div>
-                    @endif
-                    <div class="row align-items-center">
-                        <div class="col-8">
-                            <h3 class="mb-0">Bobot Saham</h3>
-                        </div>
-                        <div class="col-4 text-right">
-                            {{-- <a href="#" class="btn btn-sm btn-primary">Add Index Saham</a> --}}
-                        </div>
-                    </div>
-                </div>
-
-                <div class="table-responsive">
-                    <table class="table align-items-center">
-                    <thead class="thead-light">
-                        <tr>
-                            <th scope="col">No</th>
-                            <th scope="col">Instrument Saham</th>
-                            <th scope="col">EPS</th>
-                            <th scope="col">ROW</th>
-                            <th scope="col">PER</th>
-                            <th scope="col">DER</th>
-                            <th scope="col"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse ($bobots as $bobot)
-                            <tr>
-                                <td scope="row">{{ ++$j }}</td>
-                                <td>{{ $bobot->instrument_saham->name }}</td>
-                                <td>{{ $bobot->w_eps }}</td>
-                                <td>{{ $bobot->w_roe }}</td>
-                                <td>{{ $bobot->w_per }}</td>
-                                <td>{{ $bobot->w_der }}</td>
                                 <td class="text-right">
                                     <div class="dropdown">
                                         <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
